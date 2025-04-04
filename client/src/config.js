5835766ce17f2fc32c6580ff68f15b04bb7bbc7f
Chandrashekar Dodali <146953506+chandu-uias@users.noreply.github.com>
2025-04-04 23:20:28 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
   // BASE_URL: "http://localhost:5001",
-   BASE_URL: "https://omnify-task.vercel.app/",
+   BASE_URL: "https://omnify-task.vercel.app",
 };
 export default config;
